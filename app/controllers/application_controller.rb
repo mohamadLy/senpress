80424886fa7338002751cbf115a88d84763bd9ae
Mohamadou <mohamadou@Mohamadous-MacBook-Pro.local>
2017-04-28 17:02:30 -0400

    Added user first name and last name on registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+
+
+  # user permition added for firstname and lastname
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #protect_from_forgery with: :exception, prepend: true
+
+    protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname], keys: [:lastname])
+    end
 end
---
+++ db/migrate/20170426021215_add_user_firstname_to_users.rb
@@ -0,0 +1,5 @@
+class AddUserFirstnameToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :firstname, :string
+  end
+end
